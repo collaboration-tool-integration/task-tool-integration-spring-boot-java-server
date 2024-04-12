bda9f51b7981c00ec0f1542c76b66572f194cdf2
junho100 <bemodesty306@naver.com>
2024-04-12 12:04:03 +0900

    Core: init project

---
+++ src/main/java/com/pbl/tasktoolintegration/TaskToolIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.pbl.tasktoolintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskToolIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskToolIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pbl/tasktoolintegration/TaskToolIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbl.tasktoolintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskToolIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
